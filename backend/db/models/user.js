def366a55ee937a132a89d61432261d4fdb1c9cf
Richard <richylee95@gmail.com>
2023-04-15 10:38:19 -0700

    added firstName,lastName to login,signup response

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -38,6 +38,12 @@ module.exports = (sequelize, DataTypes) => {
       validate: {
         len: [60, 60]
       }
+    },
+    firstName:{
+      type:DataTypes.STRING,
+    },
+    lastName:{
+      type: DataTypes.STRING,
     }
   }, {
     sequelize,
