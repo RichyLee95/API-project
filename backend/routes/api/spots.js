dd64432f57d6a6afedce47f6bc51de06849516c1
Richard <richylee95@gmail.com>
2023-04-19 15:42:30 -0700

    finished get spot by user, id, create a spot, also added ondelete cascade on migrations

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -8,7 +8,7 @@ const { Spot, SpotImage, User } = require('../../db/models');
 const router = express.Router();
 
 //get all spots not finished yet
-router.get('/', async (req,res)=>{
+router.get('/', async (req, res) => {
 
 })
 
@@ -16,15 +16,70 @@ router.get('/', async (req,res)=>{
 //get all spots by current user
 router.get('/current', async (req, res) => {
     const userId = req.user.id
-    const currentSpot = await Spot.findAll({
+    const spot = await Spot.findAll({
         where: {
             ownerId: userId,
         },
     });
-    return res.json(currentSpot)
+    console.log(userId)
+    return res.json(spot)
 })
 
 
+//get spot by id
+router.get('/:spotId', async (req, res) => {
+    const spotId = req.params.spotId
+    const spot = await Spot.findAll({
+        where: {
+            id: spotId,
+        },
+        include: [{
+            model: SpotImage
+        },
+        {
+            model: User
+        },
+        ],
 
+    });
+    return res.json(spot)
+})
+
+//Create a spot
+router.post('/', async (req, res) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body
+    const newSpot = await Spot.create({
+        address, city, state, country, lat, lng, name, description, price
+    })
+    res.json({
+        data: newSpot
+    })
+})
+
+//Add img to spot based on spotid
+router.post('/:spotId/images', async (req, res) => {
+
+
+
+})
+
+//Edit a spot
+router.put('/:spotId', async (req, res) => {
+const updatedSpot = await Spot.findbyPk(req.params.spotId)
+const {address, city, state, country, lat, lng, name, description, price} = req.body
+
+await updatedSpot.save();
+res.json({
+    data:updatedSpot
+})
+})
+
+
+//Delete a spot
+router.delete('/:spotId', async (req, res) => {
+// const deletedSpot = 
+
+
+})
 
 module.exports = router;
